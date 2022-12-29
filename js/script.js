af598920510d1a30dc35de5309aab6a3473a43ad
Pallavi <dpallavi854@gmail.com>
2022-12-29 20:35:05 +0530

    added go to top button

--- js/script.js
+++ js/script.js
@@ -33,6 +33,25 @@ $("#hex").on("click", function () {
 });
 
 
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // Light/Dark toggle
 const checkbox = document.getElementById('checkbox');
 
